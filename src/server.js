2ea51c7cb17a518fee5159997e6784776e585783
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-04-09 20:24:47 +0100

    build node and express server

---
+++ src/server.js
@@ -0,0 +1,30 @@
+//import express
+import express from "express";
+
+// import body parser
+import bodyParser from "body-parser";
+
+// import cors
+import cors from "cors";
+
+// import routes
+import appRouter from "./routes";
+
+//initialize express
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.json({
+    extended: true
+}))
+
+app.use(appRouter);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=>{
+    console.log(`app is listening on port localhost:${PORT}`)
+})
+
+//exporting app for testing purposes
+export default app;
