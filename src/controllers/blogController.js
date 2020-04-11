16b4b5a3571fd5356ece4109a28cb9f34d345202
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-04-11 23:44:41 +0100

    ft(feature): controllers for adding a blog

---
+++ src/controllers/blogController.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import jsonFormatter from '../helpers/jsonFormat';
+import pool from '../models/index';
+dotenv.config();
+
+class Controller{
+    static async addBlog (req, res){
+        const title = req.body.title;
+        const category = req.body.category;
+        const picture = req.body.picture;
+        const date = req.body.date;
+        const time = req.body.time;
+        const story = req.body.story;
+        if(!title || !category || !picture || !date || !time || !story ){
+            return jsonFormatter.error(res, 'All fields are required !', 400);
+        }
+        try {
+            const query = `INSERT INTO blog(title, category, picture, date, time, story, timestamp) VALUES($1, $2, $3, $4, $5, $6, CURRENT_DATE) RETURNING *`
+            const value = [title, category, picture, date, time, story]
+            const newBlog = await pool.query(query, value);
+            return jsonFormatter.success(res, 'blog posted', newBlog.rowCount, newBlog.rows);
+        }catch(error){
+            console.error(error)
+        }
+    }
+
+}
+
+export default Controller;
