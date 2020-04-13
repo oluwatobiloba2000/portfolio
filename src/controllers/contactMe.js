7d63525fb626e5bcfeb4693c22496f6b8f6a1bce
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-04-13 14:38:22 +0100

    impor uuid and modified controllers

--- src/controllers/contactMe.js
+++ src/controllers/contactMe.js
@@ -1,17 +1,18 @@
 import jwt from 'jsonwebtoken';
+import {uuid} from 'uuidv4'
 import dotenv from 'dotenv';
 import jsonFormatter from '../helpers/jsonFormat';
 import pool from '../models/index';
 dotenv.config();
 
-class Controller{
-    static async sendMessage (req, res){
+class Controller {
+    static async sendMessage(req, res) {
         const name = req.body.name;
         const message = req.body.message;
         const subject = req.body.subject;
         const senderEmailAddress = req.body.senderEmailAddress;
         const timeReceived = req.body.timeReceived;
-        if(!name || !message || !subject || !senderEmailAddress || !timeReceived ){
+        if (!name || !message || !subject || !senderEmailAddress || !timeReceived) {
             return jsonFormatter.error(res, 'All fields are required !', 400);
         }
         try {
@@ -19,36 +20,42 @@ class Controller{
             const value = [name, message, subject, senderEmailAddress, timeReceived]
             const newMessage = await pool.query(query, value);
             return jsonFormatter.success(res, 'message sent', newMessage.rowCount, newMessage.rows);
-        }catch(error){
+        } catch (error) {
             console.error(error)
         }
     }
 
-    static async GetMessage (req, res){
+    static async GetMessage(req, res) {
         try {
             const query = `SELECT * from contactMe WHERE trash = 'false' ORDER BY TIMESTAMP`
             const messages = await pool.query(query);
-            if(!messages.rows.length) return jsonFormatter.success(res, 'empty');
+            if (!messages.rows.length) return jsonFormatter.success(res, 'empty');
             return jsonFormatter.success(res, 'All messages', messages.rowCount, messages.rows);
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
     }
 
-    //To view read and unread messages
-    static async FilterMessages(req, res){
+    //To view read and unread messages, fill the body with the word "read messages" or "unread messages"
+    static async FilterMessages(req, res) {
         const filterMessage = req.body.filterMessage;
-        if(!filterMessage == 'true' || !filterMessage == 'false') return jsonFormatter.error(res, 'Boolean field needed true or false', 400);
-        try {
-            const query = `SELECT * from contactMe WHERE read=$1`
-            const value = [filterMessage]
-            const messages = await pool.query(query, value);
-            if(!messages.rows.length) return jsonFormatter.success(res, 'empty');
-            return jsonFormatter.success(res,  filterMessage == 'true' ? 'Read messages' : 'Unread messages', messages.rowCount, messages.rows);
-        }catch(error){
-            console.log(error)
+        const filterMessageRequest = filterMessage == "read messages" ? "true" : "false";
+        console.log(filterMessageRequest)
+        if (filterMessageRequest == "true" || filterMessageRequest == "false") {
+            try {
+                const query = `SELECT * from contactMe WHERE read=$1 ORDER BY TIMESTAMP`
+                const value = [filterMessageRequest]
+                const messages = await pool.query(query, value);
+                if (!messages.rows.length) return jsonFormatter.success(res, 'empty');
+                return jsonFormatter.success(res, filterMessage == 'true' ? 'Read messages' : 'Unread messages', messages.rowCount, messages.rows);
+            } catch (error) {
+                console.log(error)
+            }
+        } else {
+            return jsonFormatter.error(res, 'field needed are "read messages" or "unread messages"', 400);
         }
     }
+    
 }
 
 export default Controller;
