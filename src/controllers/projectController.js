87c99f7d977df5660433018c2b94e67afe5596cc
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-04-17 23:31:47 +0100

    modified status code and added more features

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -2,24 +2,33 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import jsonFormatter from '../helpers/jsonFormat';
 import pool from '../models/index';
+import { uuid } from 'uuidv4';
 dotenv.config();
 
 class Controller{
     static async addProject (req, res){
         jwt.verify(req.token, process.env.SPECIAL_PIN_KEY, async (err, authorizedData)=>{
             if(err){
-                return res.json(err)
+                return res.status(403).json(err)
               }else{
+                  const id = uuid();
                   const picture = req.body.picture;
                   const hostedLink = req.body.hostedLink;
                   const githubLink = req.body.githubLink;
+                 const  projectName = req.body.projectName;
+                 const projectStatus =req.body.projectStatus;
+                 const clientCompany = req.body.clientCompany;
+                 const projectLeader = req.body.projectLeader;
+                 const estimatedBudget = req.body.estimatedBudget;
+                 const totalAmountSpent = req.body.totalAmountSpent;
+                 const estimatedProjectDuration = req.body.estimatedProjectDuration;
                   const moreDetails = req.body.moreDetails;
-                  if(!picture || !hostedLink || !githubLink || !moreDetails){
+                  if(!projectStatus || !projectName || !picture || !hostedLink || !githubLink || !moreDetails || !estimatedProjectDuration){
                       return jsonFormatter.error(res, 'All fields are required !', 400);
                   }
                   try {
-                      const query = `INSERT INTO project(picture, hostedLink, githubLink, moreDetails, timestamp) VALUES($1, $2, $3, $4, CURRENT_TIMESTAMP) RETURNING *`
-                      const value = [picture, hostedLink, githubLink, moreDetails]
+                      const query = `INSERT INTO project(id , picture, hostedLink, githubLink, projectName,clientCompany, projectLeader, estimatedBudget ,projectStatus,totalAmountSpent, estimatedProjectDuration, moreDetails, timestamp) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12,CURRENT_TIMESTAMP) RETURNING *`
+                      const value = [id, picture, hostedLink, githubLink, projectName, clientCompany , projectLeader, estimatedBudget ,projectStatus,totalAmountSpent, estimatedProjectDuration, moreDetails]
                       const newProject = await pool.query(query, value);
                       return jsonFormatter.success(res, 'project posted', newProject.rowCount, newProject.rows, 201);
                   }catch(error){
@@ -42,7 +51,7 @@ class Controller{
     static async updateProject (req, res){
         jwt.verify(req.token, process.env.SPECIAL_PIN_KEY, async (err, authorizedData)=>{
             if(err){
-                return res.json(err)
+                return res.status(403).json(err)
               }else{
                   const id = req.params.id;
                   try{
@@ -54,9 +63,16 @@ class Controller{
                       const picture = req.body.picture || formerProjectToUpdate.picture;
                       const hostedLink = req.body.hostedLink || formerProjectToUpdate.hostedLink;
                       const githubLink = req.body.githubLink || formerProjectToUpdate.githubLink;
+                      const  projectName = req.body.projectName || formerProjectToUpdate.projectName;
+                      const projectStatus =req.body.projectStatus || formerProjectToUpdate.projectStatus;
+                      const clientCompany = req.body.clientCompany || formerProjectToUpdate.clientCompany;
+                      const projectLeader = req.body.projectLeader || formerProjectToUpdate.projectLeader;
+                      const estimatedBudget = req.body.estimatedBudget || formerProjectToUpdate.estimatedBudget;
+                      const totalAmountSpent = req.body.totalAmountSpent || formerProjectToUpdate.totalAmountSpent;
+                      const estimatedProjectDuration = req.body.estimatedProjectDuration || formerProjectToUpdate.estimatedProjectDuration;
                       const moreDetails = req.body.moreDetails || formerProjectToUpdate.moreDetails;
-                      const updatequery = `UPDATE project SET picture=$1, hostedLink=$2, githubLink=$3 , moreDetails=$4 WHERE id=$5 RETURNING *`
-                      const updateValues = [picture, hostedLink, githubLink, moreDetails, id];
+                      const updatequery = `UPDATE project SET picture=$1, hostedLink=$2, githubLink=$3 ,projectName=$4,clientCompany=$5, projectLeader=$6, estimatedBudget=$7 ,projectStatus=$8,totalAmountSpent=$9, estimatedProjectDuration=$10, moreDetails=$11 WHERE id=$12 RETURNING *`
+                      const updateValues = [picture, hostedLink, githubLink, projectName,clientCompany, projectLeader, estimatedBudget ,projectStatus,totalAmountSpent, estimatedProjectDuration, moreDetails, id];
                       const newProject = await pool.query(updatequery, updateValues);
                       return jsonFormatter.success(res, 'skill updated', newProject.rowCount, newProject.rows);
                   }catch(e){
@@ -68,7 +84,7 @@ class Controller{
     static async deleteProject (req, res){
         jwt.verify(req.token, process.env.SPECIAL_PIN_KEY, async (err, authorizedData)=>{
             if(err){
-                return res.json(err)
+                return res.status(403).json(err)
               }else{
                   const id = req.params.id;
                   
