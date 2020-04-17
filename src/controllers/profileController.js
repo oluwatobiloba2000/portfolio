f045dd3e761a4f5c6f3e75afe337c4690ad34c47
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-04-17 02:14:36 +0100

    ft(feature): update profile controller

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -43,7 +43,36 @@ class Controller{
         }
     }
 
-
+    static async updateProfile(req, res){
+        jwt.verify(req.token, process.env.SPECIAL_PIN_KEY, async (err, authorizedData)=>{
+            if(err){
+                return res.json(err)
+              }else{
+                  const id = req.params.id;
+          
+                  try{
+                      const query = `SELECT * FROM profile WHERE id=$1`
+                      const value = [id];
+                      const formerProfile = await pool.query(query, value);
+                      if(!formerProfile.rows.length) return jsonFormatter.error(res, 'profile not found', 404)
+                      const formerProfileToUpdate = formerProfile.rows[0];
+                      const name = req.body.name   || formerProfileToUpdate.name;
+                      const about = req.body.about  || formerProfileToUpdate.about;
+                      const profilePics = req.body.profilePics  || formerProfileToUpdate.profilePics;
+                      const backgroundPics = req.body.backgroundPics  || formerProfileToUpdate.backgroundPics;
+                      const birthday = req.body.birthday  || formerProfileToUpdate.birthday;
+                      const phoneNumber = req.body.phoneNumber  || formerProfileToUpdate.phoneNumber;
+                      const cvLink = req.body.cvLink  || formerProfileToUpdate.cvLink;
+                      const dateLunched = req.body.dateLunched  || formerProfileToUpdate.dateLunched;
+                      const updatequery = `UPDATE profile SET name=$1, about=$2, profilePics=$3, backgroundPics=$4, birthday=$5, phoneNumber=$6, cvLink=$7, dateLunched=$8 WHERE id=$9 RETURNING *`
+                      const updateValues = [name, about, profilePics, backgroundPics, birthday, phoneNumber, cvLink, dateLunched, id];
+                      const newProfile = await pool.query(updatequery, updateValues);
+                      return jsonFormatter.success(res, 'profile updated', newProfile.rowCount, newProfile.rows);
+                  }catch(e){
+                      console.error(e)
+                  }
+              }})
+    }
 }
 
 export default Controller;
