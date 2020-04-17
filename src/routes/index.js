0d2fb91b6ce0ec1b89212d3acf130cfac19d231b
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-04-17 01:40:33 +0100

    imported all controllers and welcome route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,20 @@
 import router from "express";
-import jsonFormatter from  '../helpers/jsonFormat'
-
+import jsonFormatter from  '../helpers/jsonFormat';
+import profileController from '../controllers/profileController';
+import skillsController from  '../controllers/skillsController';
+import projectController from '../controllers/projectController';
+import blogController from '../controllers/blogController';
+import contactMe from '../controllers/contactMe';
+import lastLogoutController from '../controllers/LastLogoutSession';
+import {Authentication}from '../auth/index';
+import {checkToken} from '../auth/index';
+import ActivityController from '../controllers/activityController';
+import LoginDetailsController from '../controllers/addLoginDetails';
+import VisitorController from '../controllers/VisitorController';
 const appRouter = router();
 
 appRouter.get('/', (req, res)=>{
-    return jsonFormatter.success(res, 'success', 200, 'my portfolio site API made with Node and express by Anani oluwatobiloba');
+    return jsonFormatter.success(res, 'success', 200, 'none' ,'my portfolio site API made with Node and express by Anani oluwatobiloba');
 })
 
 export default appRouter;
