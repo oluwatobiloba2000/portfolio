a3c546b5cd677a6921b71742ce24c98c616326dd
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-04-17 02:19:33 +0100

    ft(feature): filter messages controller

--- src/controllers/contactMe.js
+++ src/controllers/contactMe.js
@@ -40,7 +40,61 @@ class Controller {
             console.log(error)
         }
     }
-
+    //To view read and unread messages, 
+    //fill the body with the word
+    // "show read messages" or "show unread messages" or "show trash messages" or "show starred messages or show unstarred messages"
+    // eg {"filterMessage": "show read messages"}
+    // you need special admin key to pass here
+    static FilterMessages(req, res) {
+        jwt.verify(req.token, process.env.SPECIAL_PIN_KEY, async (err, authorizedData)=>{
+            if(err){
+              return res.json(err)
+            }else{
+                var d = new Date(authorizedData.iat * 1000).toString();
+                var e = new Date(authorizedData.exp * 1000).toString();
+                console.log('issued at', d)
+                console.log('expried at', e)
+                let filterMessage = req.body.filterMessage;
+                if (filterMessage == 'show read messages' || filterMessage == 'show unread messages') {
+                    let filterMessageRequest = filterMessage == "show read messages" ? "true" : "false";
+                    try {
+                        const query = `SELECT * from contactMe WHERE read=$1 ORDER BY TIMESTAMP`
+                        const value = [filterMessageRequest]
+                        const messages = await pool.query(query, value);
+                        if (!messages.rows.length) return jsonFormatter.success(res, 'empty');
+                        return jsonFormatter.success(res, filterMessage == 'true' ? 'Read messages' : 'Unread messages', messages.rowCount, messages.rows);
+                    } catch (error) {
+                        console.log(error)
+                    }
+                } else if(filterMessage == 'show trash messages') {
+                   const trashMessages = 'true';
+                   try{
+                       const query = `SELECT * from contactMe WHERE trash=$1 ORDER BY TIMESTAMP`
+                       const value = [trashMessages]
+                       const messages = await pool.query(query, value);
+                       if (!messages.rows.length) return jsonFormatter.success(res, 'empty');
+                       return jsonFormatter.success(res, 'trash messages', messages.rowCount, messages.rows);
+                   }catch(e){
+                       console.error(e)
+                   }
+                }if (filterMessage == 'show starred messages' || filterMessage == 'show unstarred messages') {
+                    let filterMessageRequest = filterMessage == "show starred messages" ? "true" : "false";
+                    try {
+                        const query = `SELECT * from contactMe WHERE star=$1 ORDER BY TIMESTAMP`
+                        const value = [filterMessageRequest]
+                        const messages = await pool.query(query, value);
+                        if (!messages.rows.length) return jsonFormatter.success(res, 'empty');
+                        return jsonFormatter.success(res, filterMessage == 'true' ? 'starred messages' : 'unstarred messages', messages.rowCount, messages.rows);
+                    } catch (error) {
+                        console.log(error)
+                    }
+                }else {
+                    return jsonFormatter.error(res, 'field needed are "show read messages" or "show unread messages" or "show trash messages" or "show starred messages" or "show unstarred messages"', 400);
+                }
+            }
+        })
+    }
+    
 }
 
 export default Controller;
