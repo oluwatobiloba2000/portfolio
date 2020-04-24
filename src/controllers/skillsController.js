9f4369ceccfaac1756597682f3f9434645ff0b9d
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-04-24 16:26:07 +0100

    ft(update): returning JSON message with action keyword for easy tracking

--- src/controllers/skillsController.js
+++ src/controllers/skillsController.js
@@ -24,13 +24,13 @@ class Controller{
                   const SkillsPics = req.body.SkillsPics;
                   const skillLinkWebsite = req.body.skillLinkWebsite;
                   if(!id || !name || !SkillsPics || !skillLinkWebsite){
-                      return jsonFormatter.error(res, 'All fields are required !', 400);
+                      return jsonFormatter.error(res, 'All fields are required !', 400, undefined, 'fields required');
                   }
                   try {
                       const query = `INSERT INTO skills(id, name, SkillsPics, skillLinkWebsite, timestamp) VALUES($1, $2, $3, CURRENT_TIMESTAMP) RETURNING *`
                       const value = [id, name, SkillsPics, skillLinkWebsite]
                       const newSkills = await pool.query(query, value);
-                      return jsonFormatter.success(res, 'skill posted', newSkills.rowCount, newSkills.rows, 201);
+                      return jsonFormatter.success(res, 'skill posted', newSkills.rowCount, newSkills.rows, 201, 'posted');
                   }catch(err){
                     log(error('Error from : src/contollers/skillsController.js - addSkills'), errorMessage(err));
                   }
@@ -38,11 +38,14 @@ class Controller{
     }
 
     static async getSkills (req, res){
+        const start = parseInt(req.query.start);
+        const count = parseInt(req.query.count);
         try{
-            const query = `SELECT * FROM skills ORDER BY TIMESTAMP`
-            const skills = await pool.query(query);
+            const query = `SELECT * FROM skills ORDER BY TIMESTAMP OFFSET($1) LIMIT($2)`
+            const value = [start, count];
+            const skills = await pool.query(query, value);
             if(!skills.rows.length) return jsonFormatter.success(res, 'empty');
-            return jsonFormatter.success(res, 'All Skills', skills.rowCount, skills.rows)
+            return jsonFormatter.success(res, 'All Skills', skills.rowCount, skills.rows, undefined, 'all')
         }catch(err){
             log(error('Error from : src/contollers/skillsController.js - getSkills'), errorMessage(err));
         }
@@ -58,7 +61,7 @@ class Controller{
                       const query = `SELECT * FROM skills WHERE id=$1`
                       const value = [id];
                       const formerSkills = await pool.query(query, value);
-                      if(!formerSkills.rows.length) return jsonFormatter.error(res, 'skills not found', 404)
+                      if(!formerSkills.rows.length) return jsonFormatter.error(res, 'skills not found', 404, undefined, 'not found')
                       const formerSkillsToUpdate = formerSkills.rows[0];
                       const name = req.body.name || formerSkillsToUpdate.name;
                       const SkillsPics = req.body.SkillsPics || formerSkillsToUpdate.SkillsPics;
@@ -66,7 +69,7 @@ class Controller{
                       const updatequery = `UPDATE skills SET name=$1, skillsPics=$2, skillLinkWebsite=$3 WHERE id=$4 RETURNING *`
                       const updateValues = [name, SkillsPics, skillLinkWebsite, id];
                       const newSkills = await pool.query(updatequery, updateValues);
-                      return jsonFormatter.success(res, 'skill updated', newSkills.rowCount, newSkills.rows);
+                      return jsonFormatter.success(res, 'skill updated', newSkills.rowCount, newSkills.rows, undefined, 'updated');
                   }catch(err){
                     log(error('Error from : src/contollers/skillsController.js - updateSkills'), errorMessage(err));
                   }
@@ -80,8 +83,8 @@ class Controller{
             const query = `DELETE FROM skills WHERE id=$1`
             const value = [id];
             const skill = await pool.query(query, value);
-            if(!skill.rowCount) return jsonFormatter.error(res, 'skills not found', 404)
-            return jsonFormatter.success(res, 'skill deleted');
+            if(!skill.rowCount) return jsonFormatter.error(res, 'skills not found', 404, undefined, 'not found')
+            return jsonFormatter.success(res, 'skill deleted', undefined, undefined, undefined, 'deleted');
         }catch(err){
             log(error('Error from : src/contollers/skillsController.js - deleteSkill'), errorMessage(err));
         }
