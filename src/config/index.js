3a2da5ca6b424cc9bc434960a44665b216e1ab08
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-04-26 00:41:40 +0100

    added cloudinary config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-
+import cloudinary from 'cloudinary';
 dotenv.config();
 
 module.exports = {
@@ -26,3 +26,11 @@ module.exports = {
         database: process.env.DB_DATABASE
     }
 }
+
+
+// cloudinary config
+export default cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
